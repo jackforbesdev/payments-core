4207f3de9b2384d620f9e1134a35dd41d3d6d51a
Jack Forbes <jack@jackforbes.co.uk>
2026-02-16 17:56:08 +0000

    ci: run tests on all branches
    Remove outdated test/idempotency files

--- src/main/java/com/jackforbes/paymentscore/service/IdempotencyMistmatchException.java
+++
@@ -1,7 +0,0 @@
-package com.jackforbes.paymentscore.service;
-
-public class IdempotencyMistmatchException extends RuntimeException {
-    public IdempotencyMistmatchException(String message) {
-        super(message);
-    }
-}
